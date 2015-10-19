1460fac415e21702c6ee56e2503c88c125bda36e
Leszek Lesner <leszek.lsner@web.de>
2015-10-19 11:37:09 +0200

    Updated to newest script version

--- netrunner-desktop-containment/netrunnerdesktop-prepare-from-upstream.sh
+++ netrunner-desktop-containment/netrunnerdesktop-prepare-from-upstream.sh
@@ -9,16 +9,12 @@ mv containments/desktop $org/netrunnerdesktop/src
 cd $org
 rm -rf $dir
 cd $org/netrunnerdesktop
+rm -rf src/plugins
 curl https://paste.kde.org/pqbtebdjg/vwtdue/raw > CMakeLists.txt
-curl https://paste.kde.org/pdfgyfhfp/ze9xmk/raw > src/package/metadata.desktop
+curl https://paste.kde.org/pjklr0d9b/cx9qtc/raw > src/package/metadata.desktop
 cd src
 curl https://paste.kde.org/pgjzwaltx/fesj84/raw | patch -p3
-find . -type f -print0 | xargs -0 sed -i 's/desktopcontainment/netrunnerdesktop/g'
-find . -type f -print0 | xargs -0 sed -i 's/desktopplugin/netrunnerdesktopplugin/g'
-find . -type f -print0 | xargs -0 sed -i 's/folderplugin/netrunnerfolderplugin/g'
+find . -type f -print0 | xargs -0 sed -i 's/add_subdirectory(plugins)//g'
+find . -type f -name CMakeLists.txt -print0 | xargs -0 sed -i 's/desktopcontainment/netrunnerdesktop/g'
 find . -type f -print0 | xargs -0 sed -i 's/org\.kde\.plasma\.folder/org.kde.netrunnerdesktop/g'
 cd ..
-mv src/plugins/desktop/desktopplugin.h src/plugins/desktop/netrunnerdesktopplugin.h
-mv src/plugins/desktop/desktopplugin.cpp src/plugins/desktop/netrunnerdesktopplugin.cpp
-mv src/plugins/folder/folderplugin.h src/plugins/folder/netrunnerfolderplugin.h
-mv src/plugins/folder/folderplugin.cpp src/plugins/folder/netrunnerfolderplugin.cpp
